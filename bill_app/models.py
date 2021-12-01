4b8850e2063914a835b987bd4abe80002294fe81
david-allison12 <dgreenal@gmail.com>
2021-11-30 23:08:31 -0500

    Created User and Bill models

--- bill_app/models.py
+++ bill_app/models.py
@@ -1,3 +1,27 @@
 from django.db import models
-
+from django.contrib.auth.models import User
+from django.db.models.fields import BooleanField
+import phonenumbers
+from phonenumber_field.modelfields import PhoneNumberField
 # Create your models here.
+
+class Profile(models.Model):
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+    phone_number = PhoneNumberField()
+    email_reminder = BooleanField(default=False)
+    text_reminder = BooleanField(default=False)
+    
+    def __str__(self):
+        return self.user
+
+
+
+class Bill (models.Model):
+    bill_type = models.CharField(max_length=100)
+    company_name = models.CharField(max_length=100, default='Company Name')
+    bill_due_date = models.CharField(max_length=100)
+    min_payment = models.DecimalField(max_digits=7, decimal_places=2)
+    user = models.ForeignKey(Profile, on_delete=models.CASCADE, default={})
+
+    def __str__(self):
+        return self.company_name
