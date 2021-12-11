ead7bb68a928844a7403278e04b00d44d94a91d7
david-allison12 <dgreenal@gmail.com>
2021-12-11 12:41:22 -0500

    Set up backend user authentication

--- bill_app/views.py
+++ bill_app/views.py
@@ -3,9 +3,28 @@ from django.http import HttpResponse
 from rest_framework import viewsets
 from .serializers import BillSerializer
 from .models import Bill, User
+from rest_framework_simplejwt.serializers import TokenObtainPairSerializer
+from rest_framework_simplejwt.views import TokenObtainPairView
+
 # Create your views here.
 def home (request):
     return HttpResponse('<h1>This is the Home Page</h1>')
+
+
+class MyTokenObtainPairSerializer(TokenObtainPairSerializer):
+    @classmethod
+    def get_token(cls, user):
+        token = super().get_token(user)
+
+        # Add custom claims
+        token['username'] = user.username
+        # ...
+
+        return token
+
+class MyTokenObtainPairView(TokenObtainPairView):
+    serializer_class = MyTokenObtainPairSerializer
+
 
 
 class BillView(viewsets.ModelViewSet):
