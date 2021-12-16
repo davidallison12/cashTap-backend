e0873e8e33c1a3be6678d89d6b81fa76ef802cfc
david-allison12 <dgreenal@gmail.com>
2021-12-15 21:45:16 -0500

    Testing secret Key for heroku push

--- cashtap_backend/settings.py
+++ cashtap_backend/settings.py
@@ -26,7 +26,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-#28qls^0d0*$n#5q1i#*u%w+)oi9s@l1vf@%p92fw6_ha-5(ma"
+# SECRET_KEY = "django-insecure-#28qls^0d0*$n#5q1i#*u%w+)oi9s@l1vf@%p92fw6_ha-5(ma"
+SECRET_KEY= os.getenv("FRONTEND_URL")
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
