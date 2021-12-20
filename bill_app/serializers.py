264e26810a26419ab7bc08ae8df589f309caca84
david-allison12 <dgreenal@gmail.com>
2021-12-19 22:18:08 -0500

    Added password inside create method

--- bill_app/serializers.py
+++ bill_app/serializers.py
@@ -40,9 +40,10 @@ class UserSerializer(serializers.ModelSerializer): #https://medium.com/django-re
             first_name=validated_data['first_name'],
             last_name=validated_data['last_name'],
             email=validated_data['email'],
+            password=make_password(validated_data['password'])
         )
         
-        user.make_password(validated_data['password'])
+       
         user.save()
         return user
     
