2b2a2ed52c7f6e2d41ba18fb75ae4349d7871ed9
david-allison12 <dgreenal@gmail.com>
2021-12-19 19:40:19 -0500

    Testing other User Serializer

--- bill_app/serializers.py
+++ bill_app/serializers.py
@@ -8,37 +8,37 @@ class BillSerializer(serializers.ModelSerializer):
         fields = ('id', 'bill_type', 'company_name', 'bill_due_date', 'min_payment', 'user')
 
 
-class UserSerializer(serializers.ModelSerializer):
-    class Meta:
-        model = User
-        fields = "__all__"
-
-        def create(self, validated_data):
-            user = super(UserSerializer, self).create(validated_data)
-            user.set_password(validated_data['password'])
-            user.save()
-            return user
-
 # class UserSerializer(serializers.ModelSerializer):
-#     # password = serializers.Charfield(write_only=True)
 #     class Meta:
 #         model = User
-#         fields = ("id", "username", "password", "first_name", "last_name", "email")
-#         # write_only_fields = ('password')
+#         fields = "__all__"
+
+#         def create(self, validated_data):
+#             user = super(UserSerializer, self).create(validated_data)
+#             user.set_password(validated_data['password'])
+#             user.save()
+#             return user
+
+class UserSerializer(serializers.ModelSerializer):
+    # password = serializers.Charfield(write_only=True)
+    class Meta:
+        model = User
+        fields = ("id", "username", "password", "first_name", "last_name", "email")
+        # write_only_fields = ('password')
 
 
-#     def create(self, validated_data):
+    def create(self, validated_data):
 
-#         user = User.objects.create_user(
-#             username=validated_data['username'],
-#             first_name=validated_data['first_name'],
-#             last_name=validated_data['last_name'],
-#             email=validated_data['email']
-#         )
+        user = User.objects.create_user(
+            username=validated_data['username'],
+            first_name=validated_data['first_name'],
+            last_name=validated_data['last_name'],
+            email=validated_data['email']
+        )
         
-#         user.set_password(validated_data['password'])
-#         user.save()
-#         return user
+        user.set_password(validated_data['password'])
+        user.save()
+        return user
     
 
 
